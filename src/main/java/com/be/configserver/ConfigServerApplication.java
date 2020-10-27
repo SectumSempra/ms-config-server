00a2425733fb8fd3d2b5490fcf4b40e4464483af
bilaleraslan <bilaleraslan@gmail.com>
2020-10-27 17:31:01 +0300

    duzenleme

--- src/main/java/com/be/configserver/ConfigServerApplication.java
+++ src/main/java/com/be/configserver/ConfigServerApplication.java
@@ -19,40 +19,42 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class ConfigServerApplication implements CommandLineRunner {
 
+    private static final String ipPrefix = "192.";
+
     @Value("${EUREKA_SERVICE_URL}")
     private String EUREKA_SERVICE_URL;
 
     public static Map<String, Object> getMap() throws Exception {
-        Map<String, Object> map = new HashMap<>();
-        String ip = getIpAdressBy10();
-        map.put("ip", ip);
-        return map;
+	Map<String, Object> map = new HashMap<>();
+	String ip = getIpAdressBy10();
+	map.put("ip", ip);
+	return map;
 
     }
 
     private static String getIpAdressBy10() throws SocketException {
-        Enumeration e = NetworkInterface.getNetworkInterfaces();
-        while (e.hasMoreElements()) {
-            NetworkInterface n = (NetworkInterface) e.nextElement();
-            Enumeration ee = n.getInetAddresses();
-            while (ee.hasMoreElements()) {
-                InetAddress i = (InetAddress) ee.nextElement();
-                if (i.getHostAddress().startsWith("10."))
-                    return i.getHostAddress();
-            }
-        }
-        return null;
+	Enumeration e = NetworkInterface.getNetworkInterfaces();
+	while (e.hasMoreElements()) {
+	    NetworkInterface n = (NetworkInterface) e.nextElement();
+	    Enumeration ee = n.getInetAddresses();
+	    while (ee.hasMoreElements()) {
+		InetAddress i = (InetAddress) ee.nextElement();
+		if (i.getHostAddress().contains(ipPrefix))
+		    return i.getHostAddress();
+	    }
+	}
+	return null;
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication application = new SpringApplication(ConfigServerApplication.class);
-        application.setDefaultProperties(getMap());
-        application.run(args);
+	SpringApplication application = new SpringApplication(ConfigServerApplication.class);
+	application.setDefaultProperties(getMap());
+	application.run(args);
 
     }
 
     @Override
     public void run(String... args) throws Exception {
-        System.err.println(EUREKA_SERVICE_URL);
+	System.err.println(EUREKA_SERVICE_URL);
     }
 }
